8444f85a1f3464ffb2a3c88aec66cb7bd99d2a21
vaibhav <vaibhavsachdeva2000@gmail.com>
2023-07-27 10:56:14 +0530

    vue.js learning

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,76 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h2 v-bind:id="headingId">Heading</h2>
+    <button v-bind:disabled="isDisabled">Bind</button>
+    <h2 class="underline">Underlined text</h2>
+    <h2 class="underline" v-bind:class="status">Status</h2>
+    <h2 v-bind:class="isPromoted && 'promoted'">Promoted Movie</h2>
+    <h2 v-bind:class="isSoldout ? 'sold-out' : 'new'">Soldout? movie</h2>
+    <h2 v-bind:class="['new', 'promoted']">Newly promoted movie</h2>
+    <h2
+      v-bind:class="[isPromoted && 'promoted', isSoldout ? 'sold-out' : 'new']"
+    >
+      Array conditional movie
+    </h2>
+    <h2
+      v-bind:class="{
+        promoted: isPromoted,
+        new: !isSoldout,
+        'sold-out': isSoldout,
+      }"
+    >
+      Object conditional movie
+    </h2>
+    <h2
+      v-bind:style="{
+        color: highlightcolor,
+        fontSize: headerSize + 'px',
+        padding: '20px',
+      }"
+    >
+      Inline Style
+    </h2>
+    <h2 v-bind:style="headerStyleObject">Style Object</h2>
+    <div v-bind:style="[baseStyleObject, successStyleObject]">
+      Success Style
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      greet: "hello",
+      name: "batman",
+      channel: "vaibhav_utuber",
+      channels: "<b>vaibhavs</b>",
+      hack: `<a href = "#" onclick = "alert('You have been hacked!')">win a prize!<a/>`,
+      headingId: "heading",
+      isDisabled: true,
+      status: "danger",
+      isPromoted: true,
+      isSoldout: false,
+      highlightcolor: "orange",
+      headerSize: 50,
+      headerStyleObject: {
+        color: "orange",
+        fontSize: "50px",
+        padding: "20px",
+      },
+      baseStyleObject: {
+        fontSize: "50px",
+        padding: "10px",
+      },
+      successStyleObject: {
+        color: "green",
+        backgroundColor: "lightgreen",
+        border: "1px solid green",
+      },
+    };
+  },
+};
 </script>
 
 <style>
@@ -22,5 +81,21 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.underline {
+  text-decoration: underline;
+}
+
+.promoted {
+  font-style: italic;
+}
+
+.new {
+  color: olivedrab;
+}
+
+.sold-out {
+  color: red;
 }
 </style>

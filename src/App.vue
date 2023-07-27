e12e6e2493547c9d9d4154467084a86037d4a081
vaibhav <vaibhavsachdeva2000@gmail.com>
2023-07-27 11:54:00 +0530

    condtional_rendering

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,21 @@
 <template>
   <div>
-    <h2 v-bind:id="headingId">Heading</h2>
-    <button v-bind:disabled="isDisabled">Bind</button>
-    <h2 class="underline">Underlined text</h2>
-    <h2 class="underline" v-bind:class="status">Status</h2>
-    <h2 v-bind:class="isPromoted && 'promoted'">Promoted Movie</h2>
-    <h2 v-bind:class="isSoldout ? 'sold-out' : 'new'">Soldout? movie</h2>
-    <h2 v-bind:class="['new', 'promoted']">Newly promoted movie</h2>
-    <h2
-      v-bind:class="[isPromoted && 'promoted', isSoldout ? 'sold-out' : 'new']"
-    >
-      Array conditional movie
-    </h2>
-    <h2
-      v-bind:class="{
-        promoted: isPromoted,
-        new: !isSoldout,
-        'sold-out': isSoldout,
-      }"
-    >
-      Object conditional movie
-    </h2>
-    <h2
-      v-bind:style="{
-        color: highlightcolor,
-        fontSize: headerSize + 'px',
-        padding: '20px',
-      }"
-    >
-      Inline Style
-    </h2>
-    <h2 v-bind:style="headerStyleObject">Style Object</h2>
-    <div v-bind:style="[baseStyleObject, successStyleObject]">
-      Success Style
-    </div>
+    <h2 v-if="num === 0">The number is zero</h2>
+    <h2 v-else-if="num < 0">The number is negative</h2>
+    <h2 v-else-if="num > 0">The number is positive</h2>
+    <h2 v-else>Not a number</h2>
+
+    <template v-if="display === true">
+      <h2>vaibhav</h2>
+      <h2>vaibhav_utuber</h2>
+      <h2>Vue</h2>
+    </template>
+
+    <h2 v-show="showElement">using v-show</h2>
+    // if the condtion is false then the v-show will be toggled // to display :
+    none and always presnt in DOM // but in case of v-if // it will not be
+    present in the dom if the condition is false
+    <h2 v-if="showElement">using v-if</h2>
   </div>
 </template>
 
@@ -42,32 +24,9 @@ export default {
   name: "App",
   data() {
     return {
-      greet: "hello",
-      name: "batman",
-      channel: "vaibhav_utuber",
-      channels: "<b>vaibhavs</b>",
-      hack: `<a href = "#" onclick = "alert('You have been hacked!')">win a prize!<a/>`,
-      headingId: "heading",
-      isDisabled: true,
-      status: "danger",
-      isPromoted: true,
-      isSoldout: false,
-      highlightcolor: "orange",
-      headerSize: 50,
-      headerStyleObject: {
-        color: "orange",
-        fontSize: "50px",
-        padding: "20px",
-      },
-      baseStyleObject: {
-        fontSize: "50px",
-        padding: "10px",
-      },
-      successStyleObject: {
-        color: "green",
-        backgroundColor: "lightgreen",
-        border: "1px solid green",
-      },
+      num: "hi",
+      display: true,
+      showElement: true,
     };
   },
 };

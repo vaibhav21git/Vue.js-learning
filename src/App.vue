9cda4e6f886f2f95e07f6346ba557bd4a88c36f1
vaibhav <vaibhavsachdeva2000@gmail.com>
2023-08-02 23:19:20 +0530

    components introduction

--- src/App.vue
+++ src/App.vue
@@ -1,94 +1,14 @@
 <template>
-  <div>
-    <h2>fullname - {{ firstName }} {{ lastName }}</h2>
-    <h2>Computed fullname -{{ fullName }}</h2>
-    <button @click="changeFullName">Change fullname</button>
-    <button @click="items.push({ id: 4, title: 'Keyboard', price: 50 })">
-      Add item
-    </button>
-    <h2>Computed Total - {{ total }}</h2>
-    <h2>Method Total - {{ getTotal }}</h2>
-    <input type="text" v-model="country" />
-
-    <template v-for="item in items" :key="item.id">
-      <h2 v-if="item.price > 100">{{ item.title }} {{ item.price }}</h2>
-    </template>
-
-    <h2 v-for="item in expensiveItems" :key="item.id">
-      {{ item.title }} {{ item.price }}
-    </h2>
-  </div>
+  <GreetM />
 </template>
 
 <script>
+import GreetM from "./components/GreetM.vue";
+
 export default {
   name: "App",
-  data() {
-    return {
-      firstName: "vaibhav",
-      lastName: "sachdeva",
-      items: [
-        {
-          id: 1,
-          title: "TV",
-          price: 100,
-        },
-        {
-          id: 2,
-          title: "Phone",
-          price: 200,
-        },
-        {
-          id: 3,
-          title: "Laptop",
-          price: 300,
-        },
-      ],
-    };
-  },
-
-  //in  methods , properties  will always be recalculated on typing also , as the page
-  // is re-rendered
-  methods: {
-    getTotal() {
-      return this.items.reduce(
-        (total, curr) => (total = total + curr.price),
-        0
-      );
-    },
-
-    changeFullName() {
-      this.fullName = "manu sachdeva";
-    },
-  },
-
-  // like methods you added the computed property
-  //computed is basically an object
-  // computed object contains a key which is a computed property
-  //what we have done is composed a new data property from a existing data property
-  // why should we use computed properties over data properties
-  //1)properties need to be rendered to multiple places
-  // computed properties are automatically calculated if their dependency changes
-  computed: {
-    fullName: {
-      get() {
-        return `${this.firstName} ${this.lastName}`;
-      },
-      set(value) {
-        const names = value.split(" ");
-        this.firstName = names[0];
-        this.lastName = names[1];
-      },
-    },
-    total() {
-      return this.items.reduce(
-        (total, curr) => (total = total + curr.price),
-        0
-      );
-    },
-    expensiveItems() {
-      return this.items.filter((item) => item.price > 100);
-    },
+  components: {
+    GreetM,
   },
 };
 </script>
---
+++ src/components/GreetM.vue
@@ -0,0 +1,11 @@
+<template>
+  <h2>hello vaibhav</h2>
+</template>
+
+<script>
+export default {
+  name: "GreetM",
+};
+</script>
+
+<style scoped></style>

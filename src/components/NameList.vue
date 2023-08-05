3080fc2227aa9302adffc0372f2cd7002eb0461d
vaibhav <vaibhavsachdeva2000@gmail.com>
2023-08-05 18:45:43 +0530

    Slot_props in vue transferring data from child to parent,

---
+++ src/components/NameList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h3 v-for="name in names" :key="name.firstName">
+      <slot :firstName="name.firstName" :lastName="name.lastName"> </slot>
+    </h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NameList",
+  data() {
+    return {
+      names: [
+        {
+          firstName: "vaibhav",
+          lastName: "Sachdeva",
+        },
+        {
+          firstName: "ram ",
+          lastName: "mishra",
+        },
+        {
+          firstName: "shyam",
+          lastName: "kumar",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped></style>

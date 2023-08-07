4be8f411f5d0fd98a1225c52c19b87d500099cc2
vaibhav <vaibhavsachdeva2000@gmail.com>
2023-08-07 22:01:15 +0530

    ok

--- src/components/ChildStyles.vue
+++ src/components/ChildStyles.vue
@@ -15,10 +15,3 @@ h4 {
   color: forestgreen;
 }
 </style>
-
-<!-- 
-parent style is evaluated at the last so if you provide separated child style 
-then also the parent style is applied their -->
-<!-- if you write scoped then this can be ignored -->
-<!-- EXCEPTION IN CASE OF ROOT ELEMENT  which is without div 
-inside just inside of the template -->
